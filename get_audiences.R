e95d9f6a37f1bb50f90d88569f8892d42612171a
favstats <fabio.votta@gmail.com>
2024-03-02 14:43:37 +0100

    Initial commit

---
+++ get_audiences.R
@@ -0,0 +1,378 @@
+# Get command-line arguments
+tf <- commandArgs(trailingOnly = TRUE)
+
+here::i_am("wtm_ky.Rproj")
+
+# setwd("template")
+# getwd()
+source("utils.R")
+# ?get_targeting
+# get_targeting("41459763029", timeframe = "LAST_90_DAYS")
+# debugonce(get_targeting)
+
+library(httr)
+library(tidyverse)
+library(lubridate)
+library(rvest)
+
+sets <- jsonlite::fromJSON("settings.json")
+
+title_txt <- read_lines("_site/_quarto.yml")
+title_txt[which(str_detect(title_txt, "title"))[1]] <-  glue::glue('  title: "{sets$dashboard}"')
+write_lines(title_txt, "_site/_quarto.yml")
+
+if(Sys.info()[["sysname"]]=="Windows"){
+  ### CHANGE ME WHEN LOCAL!
+  tf <- "30"
+  print(paste0("TF: ", tf))
+}
+
+jb <- get_targeting("7860876103", timeframe = glue::glue("LAST_90_DAYS"))
+
+new_ds <- jb %>% arrange(ds) %>% slice(1) %>% pull(ds)
+# new_ds <- "2023-01-01"
+
+try({
+  latest_elex <- readRDS(paste0("data/election_dat", tf, ".rds"))
+})
+
+if(!exists("latest_elex")){
+  latest_elex <- tibble()
+}
+
+if(!("ds" %in% names(latest_elex))){
+  latest_elex <- latest_elex %>% mutate(ds = "")
+}
+
+latest_ds <- latest_elex %>% arrange(ds) %>% slice(1) %>% pull(ds)
+
+if(length(latest_ds)==0){
+  latest_ds <- "2023-01-01"
+}
+
+
+tstamp <- Sys.time()
+
+write_lines(lubridate::as_date(tstamp), "tstamp.txt")
+
+# - name: Set timeframe 
+# run: |
+#   echo "::set-env name=TIMEFRAME::30 Timeframe"
+
+
+
+
+# tstamp <- Sys.time()
+
+country_codes <- c("AD", "AL", "AM", "AR", "AT", 
+                   "AU", "BA", "BE", "BG", "BR", 
+                   "CA", "CH", "CL", "CO", "CY", 
+                   "CZ", "DE", "DK", "EC", "EE", 
+                   "ES", "FI", "FR", "GB", "GR", 
+                   "GT", "HR", "HU", "IE", "IN", 
+                   "IS", "IT", "LI", "LT", "LU", 
+                   "LV", "MD", "ME", "MK", "MT",
+                   "MX", "NL", "NO", "NZ", "PL", 
+                   "PT", "RO", "RS", "SE", "SI", 
+                   "SK", "SM", "TR", "UA", "US", 
+                   "VE", "ZA")
+
+try({
+  download.file(paste0("https://data-api.whotargets.me/advertisers-export-csv?countries.alpha2=", str_to_lower(sets$cntry)), destfile = "data/wtm_advertisers.csv")
+  
+  thedat <- read_csv("data/wtm_advertisers.csv")
+  
+})
+
+if(!exists("thedat")){
+  thedat <- tibble(no_data = NULL)
+}
+
+
+if(sets$cntry %in% country_codes & nrow(thedat)!=0){
+  
+  wtm_data <- read_csv("data/wtm_advertisers.csv") %>% #names
+    select(page_id = advertisers_platforms.advertiser_platform_ref,
+           page_name = name, party = entities.short_name)  %>%
+    mutate(page_id = as.character(page_id)) %>% 
+    mutate(sources = "wtm")
+  
+} else {
+  wtm_data <-  tibble(no_data = T)
+}
+
+polsample <- readRDS("data/polsample.rds")
+
+tep_dat <- polsample %>% 
+  filter(cntry %in% sets$cntry) %>% 
+  mutate(sources = "tep") %>% 
+  rename(party = name_short)
+
+try({
+  
+  out <- sets$cntry %>% 
+    map(~{
+      .x %>% 
+        paste0(c("-yesterday", "-last_7_days", "-last_30_days", 
+                 "-last_90_days"))
+    }) %>% 
+    unlist() %>% 
+    .[str_detect(., "last_90_days")] %>% 
+    # .[100:120] %>% 
+    map_dfr_progress(~{
+      the_assets <- httr::GET(paste0("https://github.com/favstats/meta_ad_reports/releases/expanded_assets/", .x))
+      
+      the_assets %>% httr::content() %>% 
+        html_elements(".Box-row") %>% 
+        html_text()  %>%
+        tibble(raw = .)   %>%
+        # Split the raw column into separate lines
+        mutate(raw = strsplit(as.character(raw), "\n")) %>%
+        # Extract the relevant lines for filename, file size, and timestamp
+        transmute(
+          filename = sapply(raw, function(x) trimws(x[3])),
+          file_size = sapply(raw, function(x) trimws(x[6])),
+          timestamp = sapply(raw, function(x) trimws(x[7]))
+        ) %>% 
+        filter(filename != "Source code") %>% 
+        mutate(release = .x) %>% 
+        mutate_all(as.character)
+    })
+  
+  
+  latest <- out  %>% 
+    rename(tag = release,
+           file_name = filename) %>% 
+    arrange(desc(tag)) %>% 
+    separate(tag, into = c("country", "timeframe"), remove = F, sep = "-") %>% 
+    filter(str_detect(file_name, "rds")) %>% 
+    mutate(day  = str_remove(file_name, "\\.rds|\\.zip") %>% lubridate::ymd()) %>% 
+    arrange(desc(day)) %>% 
+    group_by(country) %>% 
+    slice(1) %>% 
+    ungroup() 
+  
+  
+  download.file(paste0("https://github.com/favstats/meta_ad_reports/releases/download/", sets$cntry,"-last_90_days/", latest$file_name), 
+                destfile = "report.rds"
+  )
+  
+  last7 <- readRDS("report.rds")%>% 
+    mutate(sources = "report") %>% 
+    mutate(party = "unknown")
+  
+  file.remove("report.rds")
+})
+
+if(!exists("last7")){
+  last7 <- tibble()
+}
+
+
+all_dat <- #read_csv("nl_advertisers.csv") %>%
+  # mutate(page_id = as.character(page_id)) %>%
+  # bind_rows(internal_page_ids) %>%
+  bind_rows(wtm_data) %>%
+  bind_rows(tep_dat) %>%
+  bind_rows(last7) %>%
+  # bind_rows(rep) %>%
+  # bind_rows(more_data %>% mutate(sources = "new")) %>%
+  # bind_rows(groenams) %>%
+  distinct(page_id, .keep_all = T) %>%
+  add_count(page_name, sort  =T) %>%
+  mutate(remove_em = n >= 2 & str_ends(page_id, "0")) %>%
+  filter(!remove_em) %>%
+  # filter(n >= 2) %>%
+  # filter(n >= 2 & str_ends(page_id, "0", negate = T)) %>%
+  select(-n) 
+
+
+# all_dat %>% filter(str_detect(page_name, "GroenLinks-PvdA"))
+
+saveRDS(all_dat, "data/all_dat.rds")
+
+source("cntry.R")
+
+# all_dat %>% filter(page_id == "492150400807824")
+
+
+
+scraper <- function(.x, time = tf) {
+  
+  # print(paste0(.x$page_name,": ", round(which(internal_page_ids$page_id == .x$page_id)/nrow(internal_page_ids)*100, 2)))
+  
+  fin <- get_targeting(.x$page_id, timeframe = glue::glue("LAST_{time}_DAYS")) %>%
+    mutate(tstamp = tstamp)
+  
+  if(nrow(fin)!=0){
+    path <- paste0(glue::glue("targeting/{time}/"),.x$page_id, ".rds")
+    # if(file.exists(path)){
+    #   ol <- read_rds(path)
+    #
+    #   saveRDS(fin %>% bind_rows(ol), file = path)
+    # } else {
+    
+    saveRDS(fin, file = path)
+    # }
+  } else {
+    fin <- tibble(internal_id = .x$page_id, no_data = T) %>%
+      mutate(tstamp = tstamp)
+  }
+  
+  # print(nrow(fin))
+  # })
+  return(fin)
+  
+}
+
+scraper <- possibly(scraper, otherwise = NULL, quiet = F)
+
+
+# if(F){
+#     # dir("provincies/7", full.names
+# }
+# da30 <- readRDS("data/election_dat30.rds")
+# da7 <- readRDS("data/election_dat7.rds")
+
+if(new_ds == latest_ds){
+  print(glue::glue("New DS: {new_ds}: Old DS: {latest_ds}"))
+  
+  ### save seperately
+  enddat <- all_dat %>% 
+    arrange(page_id) %>%
+    # slice(1:150) %>% 
+    filter(!(page_id %in% latest_elex$page_id))  %>% 
+    filter(page_id %in% last7$page_id) %>% 
+    split(1:nrow(.)) %>%
+    map_dfr_progress(scraper) 
+  
+  if(nrow(enddat)==0){
+    election_dat <- latest_elex
+  } else {
+    
+    print(glue::glue("New DS: {new_ds}: Old DS: {latest_ds} 2"))
+    
+    
+    election_dat  <- enddat %>%
+      mutate_at(vars(contains("total_spend_formatted")), ~parse_number(as.character(.x))) %>% 
+      rename(page_id = internal_id) %>%
+      left_join(all_dat) %>% 
+      bind_rows(latest_elex)    
+    
+    current_date <- paste0("historic/",  as.character(new_ds), "/", tf)
+    
+    saveRDS(election_dat, file = paste0(current_date, ".rds"))
+  }
+  
+  
+} else {
+  
+  ### save seperately
+  election_dat <- all_dat %>% 
+    arrange(page_id) %>%
+    # slice(1:50) %>%
+    split(1:nrow(.)) %>%
+    map_dfr_progress(scraper)  %>%
+    mutate_at(vars(contains("total_spend_formatted")), ~parse_number(as.character(.x))) %>% 
+    rename(page_id = internal_id)  %>%
+    left_join(all_dat) 
+  
+  dir.create(paste0("historic/",  as.character(new_ds)), recursive = T)
+  current_date <- paste0("historic/",  as.character(new_ds), "/", tf)
+  
+  saveRDS(election_dat, file = paste0(current_date, ".rds"))
+  
+  
+}
+
+saveRDS(election_dat, paste0("data/election_dat", tf, ".rds"))
+
+##### combinations ####
+
+
+minimum_date <- dir("historic", recursive = T) %>%
+  keep(~str_detect(.x, paste0(tf, "\\.rds"))) %>% 
+  str_remove("/.*") %>%
+  as.Date() %>%
+  min(na.rm = T)
+
+
+if("ds" %in% names(election_dat) ){
+  
+  try({
+    
+    
+    
+    latest_ds <- election_dat %>% arrange(ds) %>% slice(1) %>% pull(ds) %>% as.Date()
+    
+    begintf <- as.Date(latest_ds) - lubridate::days(tf)
+    
+    date_vector <- vector()
+    current_date <- latest_ds
+    index <- 1
+    
+    while(current_date > minimum_date) {
+      
+      date_vector[index] <- current_date
+      
+      current_date <- current_date - lubridate::days(tf)
+      
+      index <- index + 1
+      
+    }
+    
+    if(length(date_vector != 0)){
+      
+      
+      combined_dat <- paste0("historic/", as_date(date_vector), "/", tf, ".rds") %>%
+        map_dfr(~{
+          if(!file.exists(.x)){
+            return(tibble(ds = as.character(begintf), missing_report = T))
+          } else {
+            readRDS(.x)
+          }
+          
+        })
+      
+      saveRDS(combined_dat, file= paste0("data/combined_dat", tf,  ".rds"))
+      
+      aggr <- combined_dat  %>%
+        # mutate(total_spend_formatted = ifelse(!is.character(total_spend_formatted), as.character(total_spend_formatted), total_spend_formatted, )) %>% 
+        mutate(total_spend = readr::parse_number(as.character(total_spend_formatted))) %>%
+        mutate(total_spend = ifelse(total_spend == 50, 50, total_spend)) %>%
+        mutate(total_spend = total_spend * total_spend_pct) %>%
+        group_by(page_id, value, type, location_type, detailed_type, custom_audience_type, is_exclusion) %>%
+        summarize(total_spend = sum(total_spend),
+                  num_ads = sum(num_ads),
+                  num_obfuscated = sum(num_obfuscated)) %>%
+        ungroup()
+      
+      saveRDS(aggr, file = paste0("data/election_dat_aggr", tf,  ".rds"))
+      
+      
+      
+      
+    }
+    
+    
+    
+    if(new_ds == latest_ds){
+      
+      unlink(paste0("targeting/", tf), recursive = T, force = T)
+      
+      dir.create(paste0("targeting/", tf))
+      
+      write_lines("_", paste0("targeting/", tf, "/", "_"))
+      
+    }
+    
+  })
+  
+}
+
+# sources("start.R")
+
+
+
+unlink("node_modules", recursive = T, force = T)
+unlink("out", recursive = T, force = T)
